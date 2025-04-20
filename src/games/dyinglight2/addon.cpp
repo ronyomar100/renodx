cd2b14eeb052d83234b1f3ffac6e4469c5a128cd
ronyomar100 <omar2014najjar@gmail.com>
2025-04-20 17:16:59 +0300

    Update addon.cpp

--- src/games/dyinglight2/addon.cpp
+++ src/games/dyinglight2/addon.cpp
@@ -71,7 +71,7 @@ renodx::utils::settings::Settings settings = {
         .section = "Tone Mapping",
         .tooltip = "Sets the value of 100% white in nits",
         .min = 48.f,
-        .max = 500.f,
+        .max = 1000.f,
     },
     new renodx::utils::settings::Setting{
         .key = "ToneMapUINits",
@@ -81,7 +81,7 @@ renodx::utils::settings::Settings settings = {
         .section = "Tone Mapping",
         .tooltip = "Sets the brightness of UI and HUD elements in nits",
         .min = 48.f,
-        .max = 500.f,
+        .max = 1000.f,
     },
     new renodx::utils::settings::Setting{
         .key = "GammaCorrection",
